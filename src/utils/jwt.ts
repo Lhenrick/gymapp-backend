3b60c1d9797ef6d6f5f22ed7af5217092204f46e
Luan Henrique <luanhenriquez10@hotmail.com>
2025-09-28 14:53:21 -0300

    change on jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,21 @@
-import jwt from 'jsonwebtoken';
-import { env } from '../config/env';
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
 
 export interface JwtPayload {
   sub: string; // userId
 }
 
-export function signJwt(payload: JwtPayload, expiresIn: string = '7d') {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn });
+function getSecret(): string {
+  if (!env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+  return env.JWT_SECRET;
+}
+
+export function signJwt(payload: JwtPayload, expiresIn: string = "7d") {
+  return jwt.sign(payload, getSecret(), { expiresIn });
 }
 
 export function verifyJwt<T = JwtPayload>(token: string): T {
-  return jwt.verify(token, env.JWT_SECRET) as T;
+  return jwt.verify(token, getSecret()) as T;
 }
