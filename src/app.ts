d72fdbfc4b55cd60d99c3c8dd42253a86c17273c
Luan Henrique <luanhenriquez10@hotmail.com>
2025-09-28 14:15:46 -0300

    cors config

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,14 @@ import morgan from "morgan";
 import routes from "./routes";
 import { errorHandler } from "./middleware/error";
 
+const allowed = [
+  "http://localhost:3000",
+  "https://gymapp-backend-three.vercel.app",
+];
+
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: allowed, credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
